995a2c5669312bdd20f4415a6a41d347cea28299
Mathankumar C <manomathan7890@gmail.com>
2026-06-08 03:15:01 +0530

    Remove menu button functionality

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
