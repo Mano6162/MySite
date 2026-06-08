57dc2531585ef3382cbe47d879dc0ac3d430e5c1
Mathankumar C <manomathan7890@gmail.com>
2026-06-08 14:51:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,38 +6,5 @@ form.addEventListener("submit", function(event) {
     form.reset();
 });
 
-const navLinks = document.querySelectorAll(".nav-links a");
-
-navLinks.forEach(link => {
-    link.addEventListener("click", function() {
-
-        navLinks.forEach(item => {
-            item.classList.remove("active");
-        });
-
-        this.classList.add("active");
-    });
-});
-
-
-const topBtn = document.getElementById("topBtn");
-
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 300) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
-});
-
-topBtn.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
-
-
-
 document.getElementById("year").textContent =
 new Date().getFullYear();
